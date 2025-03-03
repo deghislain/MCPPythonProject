9da6c59c7c6ea4d81d5b8989169a38f556ec71cc
deghislain@gmail.com <deghislain@gmail.com>
2025-03-03 18:26:23 -0400

    Code refactoring using langchain framework

--- mcpstockproject/MCP_stock_app_client.py
+++ mcpstockproject/MCP_stock_app_client.py
@@ -1,23 +1,73 @@
-from mcp import ClientSession, StdioServerParameters
-from mcp.client.stdio import stdio_client
+from langchain_mcp_tools import convert_mcp_to_langchain_tools
 import asyncio
+import logging
+import sys
+from utils import load_config
 import streamlit as st
-from MCP_stock_agent import StockAgent
 
-server_params = StdioServerParameters(
-    command="python",  # Executable
-    args=["MCP_stock_app_server.py"],  # Optional command line arguments
-    env=None  # Optional environment variables
-)
+config = load_config()
+dep_config = config["deployment"]
+
+try:
+    from langchain.chat_models import init_chat_model
+    from langchain.schema import HumanMessage
+    from langgraph.prebuilt import create_react_agent
+except ImportError as e:
+    print(f'\nError: Required package not found: {e}')
+    print('Please ensure all required packages are installed\n')
+    sys.exit(1)
+
+def init_logger() -> logging.Logger:
+    logging.basicConfig(
+        level=logging.INFO,  # logging.DEBUG,
+        format='\x1b[90m[%(levelname)s]\x1b[0m %(message)s'
+    )
+    return logging.getLogger()
 
 
 async def get_income_statement_info(prompt: str) -> str:
-    async with stdio_client(server_params) as (read, write):
-        async with ClientSession(read, write) as session:
-            # Initialize the connection
-            await session.initialize()
-            stock_agent = StockAgent(session)
-            return await stock_agent.run(prompt)
+    try:
+        mcp_configs = {
+            'stock_tools': {
+                'command': 'python',
+                'args': [
+                    '-m',
+                    'MCP_stock_app_server'
+                ]
+            }
+        }
+
+        tools, cleanup = await convert_mcp_to_langchain_tools(
+            mcp_configs,
+            init_logger()
+        )
+
+        llm = init_chat_model(
+            model="claude-3-5-sonnet-20241022",
+            model_provider='anthropic',
+            api_key=dep_config["ANTHROPIC_API_KEY"],
+            temperature=0,
+            max_tokens=1000
+        )
+
+        agent = create_react_agent(
+            llm,
+            tools
+        )
+
+        messages = [HumanMessage(content=prompt)]
+
+        result = await agent.ainvoke({'messages': messages})
+
+        result_messages = result['messages']
+        # the last message should be an AIMessage
+        response = result_messages[-1].content
+
+    finally:
+        if cleanup is not None:
+            await cleanup()
+    return response
+
 
 
 if __name__ == "__main__":
