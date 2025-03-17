6bda35508920582f63311e7673da52ca2dd88923
deghislain@gmail.com <deghislain@gmail.com>
2025-03-17 09:39:48 -0300

    improved prompt

--- mcpstockproject/MCP_stock_prompt.py
+++ mcpstockproject/MCP_stock_prompt.py
@@ -14,8 +14,8 @@ def get_analyse_prompt():
     Given a financial report, your task is to craft a detailed financial analyse document that clearly communicates key 
     financial metrics, analyzes trends, and provides commentary that is both thorough and relevant to investors. 
     Use the generate pdf tool to generate a pdf document that content the result of your analyze. 
-    Ensure that the report is accurate, comprehensive, and engaging
-    In your report, please include a confidence score (1-5) indicating the likelihood of long-term price appreciation
+    Ensure that the report is accurate, comprehensive, and engaging.
+    In your report, please include a confidence score (1-5) indicating the likelihood of long-term price appreciation.
     Ensure your analysis is accurate, thorough, engaging, and relevant to investors considering buying or holding a given stock. 
     Use data-driven insights and avoid speculative language.
         """
